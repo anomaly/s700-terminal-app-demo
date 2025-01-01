173ec63db041ec0fc5bafd60f5b4badb5e63270b
Dev Mukherjee <dev@anomaly.net.au>
2025-01-01 16:27:26 +1100

    refactor: basic terminal delegate lifecycle working

--- app/src/main/java/ltd/anomaly/s700demo/MainActivity.kt
+++ app/src/main/java/ltd/anomaly/s700demo/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ltd.anomaly.s700demo.ui.theme.S700DemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,6 +40,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+    // Insert a new clickable button
+    Button(
+        onClick = { /* Do something! */ },
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Text("Click me!")
+    }
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/ltd/anomaly/s700demo/S700DemoApp.kt
@@ -0,0 +1,15 @@
+package ltd.anomaly.s700demo
+
+import android.app.Application
+import android.util.Log
+import com.stripe.stripeterminal.TerminalApplicationDelegate
+
+class S700DemoApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // Print a message to the log
+        Log.e("Hello", "World")
+        TerminalApplicationDelegate.onCreate(this)
+    }
+}
